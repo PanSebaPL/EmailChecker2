cdf5518e0decb28d656e6ba463a78ea5b5cb0ef2
PanSeba <panseba.mc@o2.pl>
2023-09-22 13:42:20 +0200

    Dodaj pliki projektów.

---
+++ EmailChecker2/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace EmailChecker2
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ EmailChecker2/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿namespace EmailChecker2
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCheckClicked(object sender, EventArgs e)
+        {
+            string ent = mailEnd.Text;
+            bool czyOk = false;
+            if (ent != null) {
+                if (ent.Contains("@")){
+                    int IndexEnt = ent.IndexOf("@");
+                    if (IndexEnt>0 && IndexEnt < ent.Length-1)
+                    {
+                        czyOk = true;
+                    }
+                } else { }
+            }
+
+            if(czyOk)
+            {
+                Weryfikacja.Text = "poprawny";
+            }
+            else {
+                Weryfikacja.Text = "zly";
+            }
+            SemanticScreenReader.Announce(Weryfikacja.Text);
+        }
+    }
+}
